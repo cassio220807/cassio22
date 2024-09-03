3fce6453edeed5813c7f941052abfa21bb92ff0b
clubprenguin <aaaaa@aaa>
2024-09-03 17:06:14 -0300

    criei o arquivo vidac.js

---
+++ vidac.js
@@ -0,0 +1,18 @@
+let n1 = 323
+let n2 = 345
+
+const div = n1 / n2
+console.log(div)
+
+
+
+
+//git init
+//git add
+//git status
+//git config --local user.name "cassio220807@outlook.com"
+//git comit -m "criei o arquivo script.js"
+//git config --local user.name "cassio220807"
+//git remote add origin <link do repositório>
+//git push <nome variavel>
+// git push --set-upstream <nome variavel> <brach>
